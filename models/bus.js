256a46b018a504ed6cdd3a258970fb773a328710
YaserAlsamsamSyr <129121840+YaserAlsamsamSyr@users.noreply.github.com>
2023-10-03 02:46:26 +0300

    Add files via upload

---
+++ models/bus.js
@@ -0,0 +1,26 @@
+const seq=require('../util/db');
+const sequelize=require('sequelize');
+    const bus=seq.define('bus',{
+        type:{
+          type:sequelize.STRING,
+          allowNull:false   
+        },
+        numOfSets:{
+            type:sequelize.INTEGER,
+            allowNull:false
+        },
+        driverName:{
+            type:sequelize.STRING,
+            allowNull:false
+        },
+        helpDriverName:{
+            type:sequelize.STRING,
+            defaultValue:'no one'
+        },
+        numberOfBus:{
+            type:sequelize.STRING,
+            allowNull:false,
+            unique:true
+        }
+    });
+module.exports=bus;
