cf39832f1b2ff28016db3b5f2b8deb5944e9bb61
YaserAlsamsamSyr <129121840+YaserAlsamsamSyr@users.noreply.github.com>
2023-10-17 00:03:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use((error,req,res,next)=>{
 });
 (async()=>{
     try{
-        await DB.sync();
+        await DB.sync({force:true});
         const server=app.listen(19991,async()=>{
             console.log('server started success with database server');
         });
